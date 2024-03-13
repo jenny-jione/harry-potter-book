aaed33314695a67868034e058a9d80b3544f2546
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-03-13 16:41:04 +0900

    refactor: 함수화

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 # Character Name Frequency Analyzer
-
 import os
 
-character = 'Malfoy'
-
 # get all txt file name in the path
-txt_files = []
-for filename in os.listdir('./src'):
-    if filename.endswith('.txt'):
-        txt_files.append(filename)
-txt_files.sort()
+def get_txt_files():
+    txt_files = []
+    for filename in os.listdir('./src'):
+        if filename.endswith('.txt') and filename.startswith('Harry'):
+            txt_files.append(filename)
+    txt_files.sort()
+    return txt_files
 
-count = 0
-print(character)
-for i, txt_file in enumerate(txt_files):
+def get_character_name():
+    with open('./src/character.txt', 'r') as f:
+        characters = [line.strip() for line in f.readlines()]
+        return characters
+
+def find_character_frequency(txt_file: str, character: str):
+    count = 0
     with open(f'./src/{txt_file}', 'r') as f:
-        tmp_cnt = 0
-        line = f.readline()
-        while line:
-            if character in line:
-                count += 1
-                tmp_cnt += 1
-            line = f.readline()
-    print(f'{i+1} :: {tmp_cnt}')
-print(f'total : {count}')
+        contents = f.read()
+        count = contents.count(character)
+    return count
+
+if __name__ == "__main__":
+    chars = get_character_name()
+
+    txt_files = get_txt_files()
+
+    for char in chars:
+        count = 0
+        for i, txt_file in enumerate(txt_files):
+            cnt = find_character_frequency(txt_file, char)
+            count += cnt
+            print(f'{i+1}: {cnt}')
+        print(f'{char}: {count}')
+        print()
 
 
 
 """
 TODO
 1. (완료) src 폴더 내의 모든 txt 파일에 대해서 수행하기. (파일 이름이 바뀌어도 접근할 수 있도록)
-2. 함수화. 캐릭터가 여러명 정의되면 각각의 캐릭터에 대해 여러번 사용해야 하기 때문에
+2. (완료) 함수화. 캐릭터가 여러명 정의되면 각각의 캐릭터에 대해 여러번 사용해야 하기 때문에
 """
